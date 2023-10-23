3e47556f79afe84231fde630f3dd9b1f8715dcbe
Danial Motevali <danial.motevali.82@gmail.com>
2023-10-23 17:54:17 +0330

    create db context

--- Shavii/Data.Ef/ShaviiDbContext.cs
+++ Shavii/Data.Ef/ShaviiDbContext.cs
@@ -1,9 +1,28 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Shavii.Entites;
 
 namespace Shavii.Data.Ef
 {
     public class ShaviiDbContext : DbContext
     {
-        public DbSet<> MyProperty { get; set; }
+        public ShaviiDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+        public DbSet<Buyer> Buyers { get; set; }
+
+        public DbSet<Address> Addresses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Address>(option =>
+            {
+                option.HasOne(x => x.Buyer)
+                        .WithMany(x => x.Addresses)
+                        .HasForeignKey(x => x.BuyerId);
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
